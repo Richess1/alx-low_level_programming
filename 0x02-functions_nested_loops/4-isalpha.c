02348e9ab342069a8cd420f5c2ba2781a045fe08
Richess1 <chrisrichess143@gmail.com>
2023-07-18 06:41:35 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,31 +1,13 @@
 #include "main.h"
 /**
- * print_sign - prints the sign of a numver
+ * _isalpha - Checks for alphabetic order
  *
- * @n: the int to check
+ * @c: alpabet to check for
  *
- * Return: 1 and prints + if n is greater than zero
- *
- * 0 and prints 0 if n is zero
- *
- * -1 and prints - if n is less than zero
+ * Return: 1 if c is a letter, 0 otherwise
  */
-int print_sign(int n)
+int _isalpha(int c)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-	}
-	return (-1);
-}
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 
+}
